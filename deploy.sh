4766e4fd893ffd5480f35ca2c1db221bdfbe726b
turgayh <turgayh@itu.edu.tr>
2019-09-28 13:00:05 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
 cd public
+
 if [ -n "$GITHUB_AUTH_SECRET" ]
 then
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
     echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+
     git config credential.helper store
-    git config user.email "turgayh@itu.edu.tr"
+    git config user.email "turgayh@users.noreply.github.com"
     git config user.name "turgayh"
 fi
+
 git add .
 git commit -m "Rebuild site"
 git push --force origin HEAD:master
